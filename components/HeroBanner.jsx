e53a895076eb4657ec35effaf1870108e45fba24
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2022-09-13 02:51:53 +0530

    commit

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link';
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+// Every import in this is for a seperate componenet and each of it is dynamic and can be managed easily by the help of sanity framework. 
+
+const HeroBanner = ({ heroBanner }) => {
     return (
-        <div>HeroBanner</div>
+        <div className="hero-banner-container">
+            <div>
+                <p className="beats-solo">{heroBanner.smallText}</p>
+                <h3>{heroBanner.midText}</h3>
+                <h1>{heroBanner.largeText}</h1>
+                <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+                <div>
+                    <a herf={'/product/${heroBanner.product}'}>
+                        <button type="button">{heroBanner.buttonText}</button>
+                    </a>
+                    <div className="desc">
+                        <h5>Description</h5>
+                        <p>{heroBanner.desc}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,54 @@
 import React from 'react'
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
-  return (
-    <>
-      HeroBanner
-      <div className="products-heading">
-        <h2>
-          BestSelling Product
-        </h2>
-        <p>
-          Different types of speakers
-        </p>
-      </div>
+const Home = ({ products, bannerData }) => (// data here is passed which is passed at the bottom 
+
+  <div>
+
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
+    <div className="products-heading">
+      <h2>
+        BestSelling Product
+      </h2>
+      <p>
+        Different types of speakers
+      </p>
+    </div>
 
 
-      <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+      {/* In products?.map((product) =>product), ?.map is used to identify and map that there is a product there */}
+    </div>
+
+    <FooterBanner />
+
+  </div>
 
 
-      Footer
-    </>
+)
 
-  )
-}
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  // fetching all the query regarding the type product product
+  // products will wait unless the client requests for this query
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+
+  // whenever getServerSideProps returns the result is populated in the respective function.
+};
+
+
+// getServerSideProps is used to fetch data from the server in nextJs unlike react it does fetch data from an api or a cms this function pre renders each request using the data returned by getServerSideProps (NOTE : IT IS AN ASYNCHRONOUS FUNCTION)
 
 export default Home
