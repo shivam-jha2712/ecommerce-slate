7532817394c5a5905f559479c42c21647675066e
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2022-12-19 13:21:47 +0530

    commit

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
-
+import { useStateContext } from '../context/StateContext'
 import { urlFor } from '../lib/client';
 // Every import in this is for a seperate componenet and each of it is dynamic and can be managed easily by the help of sanity framework. 
 
 const HeroBanner = ({ heroBanner }) => {
+    const { setShowCart } = useStateContext();
     return (
         <div className="hero-banner-container">
             <div>
@@ -13,8 +14,8 @@ const HeroBanner = ({ heroBanner }) => {
                 <h1>{heroBanner.largeText}</h1>
                 <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
                 <div>
-                    <a herf={'/product/${heroBanner.product}'}>
-                        <button type="button">{heroBanner.buttonText}</button>
+                    <a herf={'/product/${heroBanner.product}'} >
+                        <button type="button" onClick={() => setShowCart(true)}>{heroBanner.buttonText}</button>
                     </a>
                     <div className="desc">
                         <h5>Description</h5>
